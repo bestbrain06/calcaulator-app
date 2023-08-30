ee7db26c8cfcf6bbb6033439e4631a2d191e472d
bestbrain06 <136890174+bestbrain06@users.noreply.github.com>
2023-08-30 09:22:46 +0000

    made the logic for the theme switcher

--- js/app.js
+++ js/app.js
@@ -1,35 +1,26 @@
 const body = document.querySelector("body");
-
 const firstTheme = document.querySelector("#first");
 const secondTheme = document.querySelector("#second");
 const thirdTheme = document.querySelector("#third");
-
-const radios = document.querySelector(".radios");
-
 const ball = document.querySelector(".ball");
-
-const screen = document.querySelector(".calculator__screen");
 
 firstTheme.addEventListener("click", () => {
   ball.style.left = "4px";
-  body.style.backgroundColor = " hsl(222, 26%, 31%)";
-  ball.style.backgroundColor = " hsl(6, 63%, 50%)";
-  radios.style.backgroundColor = "hsl(223, 31%, 20%)";
-  screen.style.backgroundColor = " hsl(224, 36%, 15%)";
+  body.classList.add("theme1");
+  body.classList.remove("theme2");
+  body.classList.remove("theme3");
 });
 
 secondTheme.addEventListener("click", () => {
   ball.style.left = "35px";
-  body.style.backgroundColor = "hsl(0, 0%, 90%)";
-  ball.style.backgroundColor = " hsl(25, 98%, 40%)";
-  radios.style.backgroundColor = "hsl(0, 5%, 81%)";
-  screen.style.backgroundColor = " hsl(0, 0%, 93%)";
+  body.classList.remove("theme1");
+  body.classList.add("theme2");
+  body.classList.remove("theme3");
 });
 
 thirdTheme.addEventListener("click", () => {
   ball.style.left = "60px";
-  body.style.backgroundColor = " hsl(268, 75%, 9%)";
-  ball.style.backgroundColor = " hsl(176, 100%, 44%)";
-  radios.style.backgroundColor = " hsl(268, 71%, 12%)";
-  screen.style.backgroundColor = "hsl(268, 71%, 12%)";
+  body.classList.remove("theme1");
+  body.classList.add("theme3");
+  body.classList.remove("theme2");
 });
